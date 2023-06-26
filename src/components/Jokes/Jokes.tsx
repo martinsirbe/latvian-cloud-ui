9ee2ad31d561242ca97b6a06aa78c5af7d4fa038
Martins Irbe <martins_irbe@mail.com>
2023-06-26 09:59:05 +0100

    Buy potatoe for this latvian guy

--- src/components/Jokes/Jokes.tsx
+++ src/components/Jokes/Jokes.tsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useJokes } from './useJokes';
 import logo from '../../assets/potatoe_cloud.png'
-import ConfettiExplosion from "react-confetti-explosion";
-
 const Joke: React.FC = () => {
     const [joke, setJoke] = useState<string | null>(null);
     const { data } = useJokes();
@@ -26,18 +24,34 @@ const Joke: React.FC = () => {
         marginTop: isSmallScreen ? '-150px' : '-65px'
     }
 
-    return <div style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignContent: 'center',
-        alignItems: 'center',
-        gap: '10px'
-    }}>
-        <ConfettiExplosion />
-        <img src={logo} alt="Potatoe Cloud" style={imageStyle}/>
-        <div style={{fontFamily: "'Oswald', sans-serif", fontSize: '24px', color: '#554924'}}>{joke}</div>
-    </div>;
+    return (
+        <>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignContent: 'center',
+                alignItems: 'center',
+                gap: '10px'
+            }}>
+                <ConfettiExplosion />
+                <img src={logo} alt="Potatoe Cloud" style={imageStyle}/>
+                <div style={{fontFamily: "'Oswald', sans-serif", fontSize: '24px', color: '#554924'}}>{joke}</div>
+            </div>
+            <a style={{
+                flexShrink: 0,
+                width: '100%',
+                textAlign: 'center',
+                padding: '10px 0',
+                fontSize: '10px',
+                color: '#b8901d'
+            }}
+                href="https://buy.a.potato.for.this.latvianguy.com">buy.a.potato.for.this.latvianguy.com
+            </a>
+        </>
+);
 };
+
+import ConfettiExplosion from "react-confetti-explosion";
 
 export default Joke;
